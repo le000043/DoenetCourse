bb7aea2262f6255a4905e7256941c7d882d53892
dat le <datle@MacBook-Pro-3.local>
2020-04-29 18:55:23 -0500

    working on selector component

--- src/React/Selector/Selection.js
+++ src/React/Selector/Selection.js
@@ -1,20 +1,37 @@
 import React, {useState } from 'react';
 // import './menu.css';
+import styled from 'styled-components';
 import {animated,useSpring} from 'react-spring';
-const Selection
+const ItemNotSelected = styled.div`
+padding-left: 5px;
+font-size: 18px;
+
+`
+const ItemSelected = styled.div `
+// padding-left: 10px;
+background-color:rgb(233, 239, 233);
+border-left: 5px solid green;
+font-size: 18px;
+
+`
 const Selection = ({callBack,label,show}) =>{
   // console.log(`show is ${show}`)
+  const [select,setSelect] = useState(false);
 return (
-  // <div>hello</div>
   <>
-  {label}
+  {select && 
+    <ItemSelected
+    onClick={()=>setSelect(!select)}
+    >{label}</ItemSelected>
+    }
+      {!select && 
+    <ItemNotSelected
+    onClick={()=>setSelect(!select)}
+    
+    >{label}</ItemNotSelected>
+    }
   
   </>
-  // (show && <div
-  // onClick={(e)=>{callBack(e)}}
-  // >
-  // {label}
-  // </div>)
 )
 };
 export default Selection;
--- src/React/Selector/SelectionSet.js
+++ src/React/Selector/SelectionSet.js
@@ -1,23 +1,50 @@
 import React, {useState } from 'react';
+import styled from 'styled-components';
+
 // import './menu.css';
 import {animated,useSpring} from 'react-spring';
+const ItemNotSelected = styled.div`
+padding-left: 10px;
+font-size: 18px;
+
+`
+const ItemSelected = styled.div `
+padding-left: 5px;
+font-size: 18px;
+
+
+background-color: gainsboro;
+border-left: 5px solid green;
+`
 const SelectionSet = ({setName,set}) =>{
+  console.log("calling SelectionSet")
   const [open,setOpen] = useState(false);
+  const [selectedItem,setSelectedItem] = useState("");
+  let updateNumber =0 ;
   let sets=[]
   Object.keys(set).map((item)=>{
     let branch = (
     <div
-    onClick={(element)=>{set[item]()}}
-    >{item}</div>)
+    key={updateNumber++}
+    style={{paddingLeft:"20px",fontSize:"18px"}}
+    onClick={(element)=>{setSelectedItem(item);set[item]()}}
+    >
+    {selectedItem==item && <ItemSelected>{item}</ItemSelected>}
+    {selectedItem!=item && <ItemNotSelected>{item}</ItemNotSelected>}
+
+    </div>)
     sets.push(branch)
   })
 return (
+  <>
   <div
-  onClick={()=>setOpen(!open)}
+    key={updateNumber++}
+  onClick={()=>{setSelectedItem("");setOpen(!open)}}
   >
   {setName}
-  {open && sets}
   </div>
+  {open && sets}
+  </>
 )
 };
 export default SelectionSet;
